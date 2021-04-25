9b121084d5d385b224fa16c8b7ee99feb51031f0
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-04-25 09:10:11 +0200

    defines new Movie model schema

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,8 +1,14 @@
-const moongose = require("mongoose");
+const moongose = require('mongoose');
+
 const { Schema, model } = moongose;
 
-const movieSchema = new Schema {
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [Number],
+});
 
-}
-
-module.exports = model("Movie", movieSchema);
+module.exports = model('Movie', movieSchema);
