35a512b0c3646f08afb28502941e63d6c9f5a0cf
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-04-26 12:38:53 +0200

    finish iteration 4, wip styles

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,24 @@ router.get('/', (req, res) => res.render('index'));
 
 /*GET movies page */
 
-router.get('/movies', (req, res, next) => {
+router.get('/movies', (req, res) => {
   Movie.find({})
         .then((moviesArray) => {
             console.log(moviesArray);
             res.render("movies", { moviesArray })
         })
-        .catch((error) => {
-            console.log(`An error occurred when trying to get the DB data: ${error}`);
-        })
+        .catch(error => console.log(`An error occurred when trying to get the DB data: ${error}`));
 });
+
+router.get("/movies/:id", (req, res) => {
+    const { id } = req.params;
+
+    Movie.findById(id)
+        .then((movieDetails) => {
+            console.log(movieDetails)
+            res.render("details", movieDetails)
+        })
+        .catch(error => console.log(`An error occurred when trying to get the DB data: ${error}`))
+})
 
 module.exports = router;
